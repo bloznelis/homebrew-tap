d9476bdd595d8148db23f96baa5b312de50933f0
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2022-05-20 17:20:45 +0300

    Release 1.1.1

--- Formula/typioca.rb
+++ Formula/typioca.rb
@@ -1,9 +1,9 @@
 class Typioca < Formula
     desc "Minimal, terminal based typing speed tester"
     homepage "https://github.com/bloznelis/typioca"
-    url "https://github.com/bloznelis/typioca/archive/refs/tags/1.1.0.tar.gz"
-    sha256 "1880357bbe3872a9f4831e25a18ecaa282610b810a43630b6d0beba2e72af867"
-    version "1.1.0"
+    url "https://github.com/bloznelis/typioca/archive/refs/tags/1.1.1.tar.gz"
+    sha256 "dc5f8b02cb77f87628fd420570a80c8891e3a7ea8b2356582d68895e16911af3"
+    version "1.1.1"
     depends_on "go"
     license "MIT"
 
