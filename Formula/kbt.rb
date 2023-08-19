369b30e176646bd7f79c4d0cebd71f6f385b3aba
Souma <101255979+5ouma@users.noreply.github.com>
2023-08-19 15:15:12 +0900

    [clean] Refactor with `brew style`
    Fix the errors when running the command.

--- Formula/kbt.rb
+++ Formula/kbt.rb
@@ -1,14 +1,14 @@
 class Kbt < Formula
-    desc "Keyboard tester in terminal"
-    homepage "https://github.com/bloznelis/kbt"
-    url "https://github.com/bloznelis/kbt/archive/1.2.3.tar.gz"
-    sha256 "7c74986c28a29a89609176a59e234ef9fd724a20d3dc904853794bd88a60112e"
-    version "1.2.3"
-    depends_on "rust"
-    license "MIT"
+  desc "Keyboard tester in terminal"
+  homepage "https://github.com/bloznelis/kbt"
+  url "https://github.com/bloznelis/kbt/archive/1.2.3.tar.gz"
+  version "1.2.3"
+  sha256 "7c74986c28a29a89609176a59e234ef9fd724a20d3dc904853794bd88a60112e"
+  license "MIT"
+  depends_on "rust"
 
-    def install
-        system "make", "build", "VERSION=#{version}"
-        bin.install "target/release/kbt"
-    end
+  def install
+    system "make", "build", "VERSION=#{version}"
+    bin.install "target/release/kbt"
+  end
 end
