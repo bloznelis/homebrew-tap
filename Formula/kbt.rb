4ae504b9fc812942fbf0d4969fbf5bb3bc3555ba
Lukas Bloznelis <33397865+bloznelis@users.noreply.github.com>
2023-11-05 13:08:32 +0200

    kbt 2.0.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/kbt.rb
+++ Formula/kbt.rb
@@ -1,9 +1,9 @@
 class Kbt < Formula
   desc "Keyboard tester in terminal"
   homepage "https://github.com/bloznelis/kbt"
-  url "https://github.com/bloznelis/kbt/archive/refs/tags/2.0.5.tar.gz"
-  version "2.0.5"
-  sha256 "e1863f6d414212df59fcaba8ea0bffc9b68b35cb1cca175b57c2d4067880c54d"
+  url "https://github.com/bloznelis/kbt/archive/refs/tags/2.0.6.tar.gz"
+  version "2.0.6"
+  sha256 "861ba04cf58eaef702f6ff7c499f5daea2090828a5628a148df5447b46bff888"
   license "MIT"
   depends_on "rust"
 
