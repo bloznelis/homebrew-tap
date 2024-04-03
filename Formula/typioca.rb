8d2f32fc7bf641d68cc5c9a2ac31feed00694d72
Lukas Bloznelis <33397865+bloznelis@users.noreply.github.com>
2024-04-03 18:38:25 +0300

    typioca 2.11.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/typioca.rb
+++ Formula/typioca.rb
@@ -1,9 +1,9 @@
 class Typioca < Formula
     desc "Minimal, terminal based typing speed tester"
     homepage "https://github.com/bloznelis/typioca"
-    url "https://github.com/bloznelis/typioca/archive/refs/tags/2.10.0.tar.gz"
-    sha256 "444dbb145a862b45a8342104d428e6d13bb7089ee5bb8531ddd87d33045079db"
-    version "2.10.0"
+    url "https://github.com/bloznelis/typioca/archive/refs/tags/2.11.0.tar.gz"
+    sha256 "61f44059e34c4fdd509566253b5f2bf45025303fdaa00fd390484da2359f2bf0"
+    version "2.11.0"
     depends_on "go"
     license "MIT"
 
